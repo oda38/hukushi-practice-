6219f08c203cdf3603df2cec76f71c3940adb78a
oda38 <[odasao.38@gmail.com]>
2023-07-08 06:35:27 +0000

    [add]

--- app/controllers/publics/homes_controller.rb
+++ app/controllers/publics/homes_controller.rb
@@ -1,4 +1,10 @@
 class Publics::HomesController < ApplicationController
   def top
+    @announcements = Announcement.order(created_at: :desc).first(3)
   end
+  
+  def announcements
+    @announcements = Announcement.order(created_at: :desc).page(params[:page]).per(3)
+  end
+  
 end
